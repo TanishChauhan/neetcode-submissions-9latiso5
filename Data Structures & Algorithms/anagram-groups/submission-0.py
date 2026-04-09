4b9f7f4c416a0eadfaf5101dbf56db998d4ad069
Tanish Chauhan <54852432+TanishChauhan@users.noreply.github.com>
2026-04-09 16:34:18 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        n=len(strs)
+        index=[-1]*n
+        arr=[]
+        
+        for i in range(n):
+            if i in index:
+                continue
+            
+            temp=[]
+            for j in range(i+1,n):
+                if sorted(strs[i])==sorted(strs[j]):
+                    temp.append(strs[j])
+                    
+                    index.append(j)
+            temp.append(strs[i])
+            if temp:        
+                arr.append(temp)
+        return arr
