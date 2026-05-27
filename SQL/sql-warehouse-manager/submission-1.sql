1f49f4927762e70195ca0f20c17c885283ac6629
Tanish Chauhan <54852432+TanishChauhan@users.noreply.github.com>
2026-05-27 06:37:35 +0530

    Add: sql-warehouse-manager - submission-1

---
+++ SQL/sql-warehouse-manager/submission-1.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+
+
+select name as warehouse_name,sum(width*length*height*units) as volume
+from warehouse w
+left join 
+products p
+on w.product_id=p.product_id
+group by name
